32a572b0c32bda195525c52f10ba3a356148fa37
Tejpratap Kushwaha <tkushwaha@XEBIAINDIA.LOCAL>
2018-02-13 15:33:09 +0530

    add anotation in stock

--- src/main/java/com/exchange/server/demoexchange/entity/StockEntity.java
+++ src/main/java/com/exchange/server/demoexchange/entity/StockEntity.java
@@ -1,9 +1,12 @@
 package com.exchange.server.demoexchange.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="stock_information")
+@Data
 public class StockEntity {
 
 
@@ -22,29 +25,5 @@ public class StockEntity {
      */
     @Column(name="stock_price")
     private String price;
-
-    public String getStockId() {
-        return stockId;
-    }
-
-    public void setStockId(String stockId) {
-        this.stockId = stockId;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
 
 }
